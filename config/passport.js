64c9b12c4c9c8a3ed01817682d5a72d0a24735f6
Boyeon Ihn <boyeonihn@gmail.com>
2023-02-11 15:37:11 +0900

    refactor: passport-local-strategy code per docs

--- config/passport.js
+++ config/passport.js
@@ -1,40 +1,80 @@
-const LocalStrategy = require('passport-local').Strategy;
+// const LocalStrategy = require('passport-local').Strategy;
+// const mongoose = require('mongoose');
+// const User = require('../models/User');
+
+const LocalStrategy = require('passport-local');
 const mongoose = require('mongoose');
 const User = require('../models/User');
 
-module.exports = function (passport) {
-  passport.use(
-    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      User.findOne({ email: email.toLowerCase() }, (err, user) => {
+passport.use(
+  new LocalStrategy(function verify(username, password, cb) {
+    User.findOne(
+      { username: username.toLowerCase()},
+      function (err, user) {
         if (err) {
-          return done(err);
+          return cb(err);
         }
         if (!user) {
-          return done(null, false, { msg: `Email ${email} not found.` });
-        }
-        if (!user.password) {
-          return done(null, false, {
-            msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.',
+          return cb(null, false, {
+            message: `username/email ${username} not found`,
           });
         }
-        user.comparePassword(password, (err, isMatch) => {
-          if (err) {
-            return done(err);
-          }
-          if (isMatch) {
-            return done(null, user);
-          }
-          return done(null, false, { msg: 'Invalid email or password.' });
-        });
-      });
-    })
-  );
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
+        crypto.pbkdf2(
+          password,
+          user.salt,
+          310000,
+          32,
+          'sha256',
+          function (err, hashedPassword) {
+            if (err) {
+              return cb(err);
+            }
+            if (!crypto.timingSafeEqual(user.hashed_password, hashedPassword)) {
+              return cb(null, false, {
+                message: 'Incorrect username or password.',
+              });
+            }
+            return cb(null, user);
+          }
+        );
+      }
+    );
+  })
+);
+// module.exports = function (passport) {
+//   passport.use(
+//     new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+//       User.findOne({ email: email.toLowerCase() }, (err, user) => {
+//         if (err) {
+//           return done(err);
+//         }
+//         if (!user) {
+//           return done(null, false, { msg: `Email ${email} not found.` });
+//         }
+//         if (!user.password) {
+//           return done(null, false, {
+//             msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.',
+//           });
+//         }
+//         user.comparePassword(password, (err, isMatch) => {
+//           if (err) {
+//             return done(err);
+//           }
+//           if (isMatch) {
+//             return done(null, user);
+//           }
+//           return done(null, false, { msg: 'Invalid email or password.' });
+//         });
+//       });
+//     })
+//   );
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
-  });
-};
+//   passport.serializeUser((user, done) => {
+//     done(null, user.id);
+//   });
+
+//   passport.deserializeUser((id, done) => {
+//     User.findById(id, (err, user) => done(err, user));
+//   });
+// };
