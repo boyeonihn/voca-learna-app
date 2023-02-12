4405a7e66c2a1c065e98d2427088a243269cfd22
Boyeon Ihn <boyeonihn@gmail.com>
2023-02-12 22:33:12 +0900

    feat: add signout/logout user feature (#10)

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,17 +61,21 @@ exports.postLogin = passport.authenticate('local', {
 //   })(req, res, next);
 // };
 
-// exports.logout = (req, res) => {
-//   req.logout(() => {
-//     console.log('User has logged out.');
-//   });
-//   req.session.destroy((err) => {
-//     if (err)
-//       console.log('Error : Failed to destroy the session during logout.', err);
-//     req.user = null;
-//     res.redirect('/');
-//   });
-// };
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    req.session.destroy((err) => {
+      if (err) {
+        console.log('Error, Failed to destroy the session during logout.', err);
+      }
+      req.user = null;
+      console.log('User has logged out');
+      res.redirect('/');
+    });
+  });
+};
 
 // exports.getSignup = (req, res) => {
 //   if (req.user) {
--- controllers/home.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  getIndex: (req, res) => {
-    try {
-      res.render('index.ejs');
-    } catch (err) {
-      if (err) return res.status(500).send(err);
-    }
-  },
-};
